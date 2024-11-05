4603395ed47c1a1a3c0229e71f4c1abdd7ada238
LAShemilt <laura.shemilt@rfi.ac.uk>
2024-11-05 09:53:34 +0000

    adding flavour to the puzzle

--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 from importlib.metadata import entry_points
-from adventurelib import start
+from adventurelib import *
 
 def lock_the_room():
     discovered_rooms = [ e.load() for e in entry_points(group="rooms")]
-    print(discovered_rooms)
 
     for room in discovered_rooms:
         room.play_room()
         # ctr-c to break
+
+
     start()
--- src/rooms/entrance.py
+++ src/rooms/entrance.py
@@ -1,13 +1,25 @@
 from escape_room import EscapeRoom
-from adventurelib import Room
+from adventurelib import Room, Item, say
 
 class EntrancePuzzle(EscapeRoom):
 
+    def __init__(self) -> None:
+        super().__init__()
+
+        # Define your room
+        self.entrance_hall = Room(""" You have entered an entrance hall.
+                                It has a high vaulted ceiling and there is an ancient desk in the corner, papers scattered over its surface.
+                                Above the desk is a clock, it has stopped. No one has been here for years. In
+                                the north you see a pair of double doors and a key pad.""")
+
+        # Define the items in your room
+        self.keypad = Item("The keypad requires a 4 digit code", "keypad")
+        self.clock = Item("The clocks hands are stuck at 17 minutes past 11", "clock")
+
 
     def play_room(self):
+        # this is where you script your game, essentially your main method. It is used in the code like the statement
+        # if __name__=='__main__'
 
-        entrance_hall = Room(""" You have entered an entrance hall.
-                             It has a high vaulted ceiling and there is an ancient desk in the corner there are some papers scattered on it.
-                             You look around, there is a clock, its hands stuck at midnight. No one has been here for years. In
-                             the north you see a pair of double doors and a key pad.""")
+        say(self.entrance_hall)
 
