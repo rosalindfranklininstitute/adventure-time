ab18a4147ea7de633ba4ab182fb1ee1920931d6e
LAShemilt <laura.shemilt@rfi.ac.uk>
2024-11-06 14:32:04 +0000

    tidying up name and changing room to puzzle to make the nomenclature easier

--- src/main.py
+++ src/main.py
@@ -1,17 +1,24 @@
 from adventurelib import say, when, start, Bag
 from adventurelib import *
 from lock import Lock
-from rooms import *
+from puzzles import *
 
-#### Setting up inventories #####
-play_inventory = Bag()
-current_lock = Bag()
+########### Setting up inventories ##################
 
+player_inventory = Bag() # Players can add Items to their bag
+current_lock = Bag() # special bag that only contains the current lock the player is trying to solve
 
-#### Useful verbs ####
+############ Define Player Actions #################
 
 @when('examine ITEM')
 def examine(item):
+    """T
+    Enter 'examine <item_name>' into game prompt to get the description of items in a room. e.g. > examine box
+
+    Args:
+        item (str): the name of the item.
+    """
+
     if item in current_room.items:
         current_item = current_room.items.find(item)
         say(current_item)
@@ -20,6 +27,14 @@ def examine(item):
 
 @when('enter KEY')
 def enter(key):
+    """
+    Enter 'enter <key>' into game prompt to try and unlock a lock, e.g. > enter abc
+    The lock is defined in the puzzle. This function uses the Lock.unlock() function
+    to return an answer
+
+    Args:
+        key (str): the code or item to unlock the lock.
+    """
     if len(current_lock)==0:
         say("You are not trying any locks. \
             Please enter 'try <lock_name>'")
@@ -32,6 +47,13 @@ def enter(key):
 
 @when('try LOCK')
 def try_lock(lock):
+    """
+    Enter 'try <lock_name>' into the game prompt select the lock to solve. Returns prompt for the lock.
+
+
+    Args:
+        lock (str): name of lock
+    """
     current_lock.clear()
     if lock in current_room.items:
         obj = current_room.items.find(lock)
@@ -43,11 +65,19 @@ def try_lock(lock):
     else:
         say(f"There is no {lock} in this room")
 
-@when('north', direction='north')
-@when('south', direction='south')
-@when('east', direction='east')
-@when('west', direction='west')
+@when('go north', direction='north')
+@when('go south', direction='south')
+@when('go east', direction='east')
+@when('go west', direction='west')
 def go(direction):
+    """
+    Type 'go <direction>' in the game prompt to move in that direction.
+       You can only exit from a particular direction, which will lead to another room.
+       If a room is locked you will not be able to go in that direction.
+
+    Args:
+        direction (str): the direction to move in
+    """
     global current_room
     room = current_room.room.exit(direction)
     if room:
@@ -62,6 +92,10 @@ def go(direction):
 
 @when('is it open')
 def is_open():
+    """
+    Type 'is it open' into game prompt to find out if a room is open.
+    Returns state of the room.
+    """
     if current_room.locked:
         say("The room is locked, continue to solve puzzles to unlock the room")
     else:
@@ -69,15 +103,24 @@ def is_open():
 
 @when('where am i')
 def whereami():
+    """
+    Enter 'where am i' into the game prompt to find the description of the you are in
+    """
     say(current_room.room)
 
+
+############################## Game Loop ##########################################
 def lock_the_room():
+
+    ######################### Add Puzzles #########################
 
     entrance = Entrance()
     staircase =  Staircase()
     store_room =  StoreRoom()
     wet_lab =  WetLab()
     microscope_hall =  MicroscopeHall()
+
+    ############### Add directional relationship #################
 
     entrance.room.north = staircase
     staircase.room.south = entrance
@@ -94,6 +137,7 @@ def lock_the_room():
 
     microscope_hall.room.west = Room("""Congratulations you have escaped from the Lab!""")
 
+    #############  Set start room and start game ####################
     global current_room
     current_room = entrance
     say(current_room.room)
