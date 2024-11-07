b0ec4a22693b60dc4379f2f6fa291c30ffd4767e
LAShemilt <laura.shemilt@rfi.ac.uk>
2024-11-07 08:58:27 +0000

    adding a debug mode so users do not have to play the whole game to test their room

---
+++ src/puzzles/finish.py
@@ -0,0 +1,23 @@
+from escape_room import EscapeRoom
+from lock import Lock
+from adventurelib import Room, Item, Bag
+
+class Finish(EscapeRoom):
+
+    def __init__(self):
+        self.name = "finish"
+
+        self.room = Room("""You find yourself on a lawn surrounded by bright sunlight. In the distance you see people wandering around the campus. You breathe a sigh of relief.Congratualtions you have escaped from the Lab!""")
+
+        self.escape_direction = None # escape direction of the room. Do not delete of change this property
+
+
+        self.locked = False # start with the room locked
+
+
+    def room_completed(self):
+
+        pass
+
+    def escape(self):
+        pass
