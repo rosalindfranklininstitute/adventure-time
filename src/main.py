42c10f0000f1492bf793b3845a57f38a8883c447
Alex Lubbock <alex.lubbock@rfi.ac.uk>
2024-11-08 14:18:53 +0000

    Wet lab room

--- src/main.py
+++ src/main.py
@@ -25,6 +25,50 @@ def examine(item):
     else:
         say(f'There is no {item} in this room')
 
+@when('take ITEM')
+def take(item):
+    TAKEABLE = ("carrot cake", "carrot", "red syringe", "blue syringe", "green syringe")
+
+    if item not in TAKEABLE:
+        say("You can't take that!")
+        return
+
+    obj = current_puzzle.items.take(item)
+    if obj:
+        say('You pick up the %s.' % obj)
+        player_inventory.add(obj)
+    else:
+        say('There is no %s here.' % item)
+
+@when('talk to ITEM')
+def talk_to(item):
+    if item == "scientist":
+        say("I really need help to immunise this llama. He won't stay still! If you help me with that, I'll give you the code to escape.")
+    else:
+        say("You can't talk to that, silly!")
+
+@when('immunise ITEM')
+def immunise(item):
+    if item == "scientist":
+        say("I don't think he'd like that!")
+        return
+    elif item != "llama":
+        say("You can't immunise that!")
+        return
+
+    if "red syringe" not in player_inventory:
+        say("You'll need the correct colour syringe for that.")
+        return
+
+    if "carrot" not in player_inventory:
+        if "carrot cake" in player_inventory:
+            say("The llama sniffs at the cake, but turns his nose up at it and spits at you. Typical.")
+            return
+        say("The llama refuses to stay still. Maybe you can placate him a carrot-based snack?")
+        return
+
+    say("You inject the llama with the red syringe. 'Thank you so much!' says the scientist. 'The code for the door is 2846'.")
+
 @when('enter KEY')
 def enter(key):
     """
