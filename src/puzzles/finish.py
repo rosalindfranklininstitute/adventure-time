4f236a388d4df42619245bd501df816954980b68
LAShemilt <laura.shemilt@rfi.ac.uk>
2024-12-03 15:09:18 +0000

    correcting typo in finish

--- src/puzzles/finish.py
+++ src/puzzles/finish.py
@@ -7,7 +7,7 @@ class Finish(EscapeRoom):
     def __init__(self):
         self.name = "finish"
 
-        self.room = Room("""You find yourself on a lawn surrounded by bright sunlight. In the distance you see people wandering around the campus. You breathe a sigh of relief.Congratualtions you have escaped from the Lab!""")
+        self.room = Room("""You find yourself on a lawn surrounded by bright sunlight. In the distance you see people wandering around the campus. You breathe a sigh of relief. Congratulations you have escaped from the Lab!""")
 
         self.escape_direction = None # escape direction of the room. Do not delete of change this property
 
